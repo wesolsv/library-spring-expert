2f649cdc91a9b3ba3ba1196474385743b1fa495e
Wesley Oliveira Silva <103519823+wesolsv@users.noreply.github.com>
2025-09-08 14:40:04 -0300

    Iniciando as configurações de segurança das apis, incluindo usuários e hypersistence

--- src/main/java/io/github/wesolsv/libraryapi/config/SecurityConfiguration.java
+++ src/main/java/io/github/wesolsv/libraryapi/config/SecurityConfiguration.java
@@ -2,6 +2,7 @@ package io.github.wesolsv.libraryapi.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -23,10 +24,17 @@ public class SecurityConfiguration {
         return http
                 .csrf(AbstractHttpConfigurer::disable)
                 .formLogin(configurer ->{
-                    configurer.loginPage("/login").permitAll();
+                    configurer.loginPage("/login");
                 })
                 .httpBasic(Customizer.withDefaults())
                 .authorizeHttpRequests(authorize -> {
+                    authorize.requestMatchers("/login").permitAll();
+                    authorize.requestMatchers(HttpMethod.POST,"/autores/**").hasRole("ADMIN");
+                    authorize.requestMatchers(HttpMethod.DELETE,"/autores/**").hasRole("ADMIN");
+                    authorize.requestMatchers(HttpMethod.PUT,"/autores/**").hasRole("ADMIN");
+                    authorize.requestMatchers(HttpMethod.GET,"/autores/**").hasAnyRole("USER", "ADMIN");
+                    authorize.requestMatchers("/livros/**").hasAnyRole("USER", "ADMIN");
+
                     authorize.anyRequest().authenticated();
                 })
                 .build();
