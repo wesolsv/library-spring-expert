5dd19a3f8dba17422f69e4fd70aa5e04c5aefe45
Wesley Oliveira Silva <103519823+wesolsv@users.noreply.github.com>
2026-01-12 20:20:21 -0300

    Finalizando autenticação e iniciando a inclusão do swagger

--- src/main/java/io/github/wesolsv/libraryapi/config/AuthorizationServerConfiguration.java
+++ src/main/java/io/github/wesolsv/libraryapi/config/AuthorizationServerConfiguration.java
@@ -5,21 +5,26 @@ import com.nimbusds.jose.jwk.RSAKey;
 import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
 import com.nimbusds.jose.jwk.source.JWKSource;
 import com.nimbusds.jose.proc.SecurityContext;
+import io.github.wesolsv.libraryapi.security.CustomAuthentication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.server.authorization.OAuth2TokenType;
 import org.springframework.security.oauth2.server.authorization.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
 import org.springframework.security.oauth2.server.authorization.config.annotation.web.configurers.OAuth2AuthorizationServerConfigurer;
 import org.springframework.security.oauth2.server.authorization.settings.AuthorizationServerSettings;
 import org.springframework.security.oauth2.server.authorization.settings.ClientSettings;
 import org.springframework.security.oauth2.server.authorization.settings.OAuth2TokenFormat;
 import org.springframework.security.oauth2.server.authorization.settings.TokenSettings;
+import org.springframework.security.oauth2.server.authorization.token.JwtEncodingContext;
+import org.springframework.security.oauth2.server.authorization.token.OAuth2TokenCustomizer;
 import org.springframework.security.web.SecurityFilterChain;
 
 import java.security.KeyPair;
@@ -27,6 +32,8 @@ import java.security.KeyPairGenerator;
 import java.security.interfaces.RSAPrivateKey;
 import java.security.interfaces.RSAPublicKey;
 import java.time.Duration;
+import java.util.Collection;
+import java.util.List;
 import java.util.UUID;
 
 @Configuration
@@ -118,5 +125,25 @@ public class AuthorizationServerConfiguration {
                 // logout
                 .oidcLogoutEndpoint("/oauth2/logout")
                 .build();
+    }
+
+    @Bean
+    public OAuth2TokenCustomizer<JwtEncodingContext> tokenCustomizer(){
+        return context -> {
+            var principal = context.getPrincipal();
+
+            if(principal instanceof CustomAuthentication authentication){
+                OAuth2TokenType tipoToken = context.getTokenType();
+
+                if(OAuth2TokenType.ACCESS_TOKEN.equals(tipoToken)){
+                    Collection<GrantedAuthority> authorities = (Collection<GrantedAuthority>) authentication.getAuthorities();
+                    List<String> authoritiesList = authorities.stream().map(GrantedAuthority::getAuthority).toList();
+                    context
+                            .getClaims()
+                            .claim("authorities", authoritiesList)
+                            .claim("email", authentication.getUsuario().getEmail());
+                }
+            }
+        };
     }
 }
--- src/main/java/io/github/wesolsv/libraryapi/config/SecurityConfiguration.java
+++ src/main/java/io/github/wesolsv/libraryapi/config/SecurityConfiguration.java
@@ -11,6 +11,7 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.core.GrantedAuthorityDefaults;
 import org.springframework.security.core.userdetails.User;
@@ -60,6 +61,18 @@ public class SecurityConfiguration {
                 .oauth2ResourceServer(oauth2Rs -> oauth2Rs.jwt(Customizer.withDefaults()))
                 .addFilterAfter(jwtCustomAuthenticationFilter, BearerTokenAuthenticationFilter.class)
                 .build();
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer(){
+        return web -> web.ignoring().requestMatchers(
+                  "/v2/api-docs/**",
+                  "/v3/api-docs/**",
+                  "/swagger-resources/**",
+                  "/swagger-ui.html/**",
+                  "/swagger-ui/**",
+                  "/webjars/**"
+        );
     }
 
     //Config prefixo da role default
